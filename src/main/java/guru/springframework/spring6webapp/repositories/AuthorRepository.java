2ab9b746065f8504a3b3e88bbf4432f62729b2d3
PhemeloMotsamai <phemelojmotsamai@gmail.com>
2023-06-14 11:03:20 +0200

    added Author Repo Comments and Book Repo Comments

--- src/main/java/guru/springframework/spring6webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring6webapp/repositories/AuthorRepository.java
@@ -3,5 +3,11 @@ package guru.springframework.spring6webapp.repositories;
 import guru.springframework.spring6webapp.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
+// This interface AuthorRepository is a repository interface for the Author entity.
+// It extends the CrudRepository interface, which provides basic CRUD operations for entities.
+// The Author entity represents an author in the application.
+// The type parameters <Author, Long> specify that the repository works with Author entities and uses Long as the type for the entity's ID.
+// By extending CrudRepository, the AuthorRepository inherits methods such as save(), findById(), findAll(), delete(), etc., which can be used to perform CRUD operations on Author entities.
+// This repository interface allows convenient interaction with the underlying database for managing Author entities.
 public interface AuthorRepository extends CrudRepository <Author,Long> {
 }
--- src/main/java/guru/springframework/spring6webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring6webapp/repositories/BookRepository.java
@@ -3,5 +3,12 @@ package guru.springframework.spring6webapp.repositories;
 import guru.springframework.spring6webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
+
+// This interface BookRepository serves as a repository for the Book entity.
+// It extends the CrudRepository interface provided by Spring Data JPA.
+// The Book entity represents a book in the application.
+// The type parameters <Book, Long> specify that the repository works with Book entities and uses Long as the type for the entity's ID.
+// By extending CrudRepository, the BookRepository inherits methods such as save(), findById(), findAll(), delete(), etc., which can be used to perform CRUD operations on Book entities.
+// This repository interface allows convenient interaction with the underlying database for managing Book entities.
 public interface BookRepository extends CrudRepository <Book,Long>{
 }
